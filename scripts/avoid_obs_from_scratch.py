c369b3271349aa07d89cb67604d9efe3a9bcf914
TicaGit <thibaud.trinca@hotmail.fr>
2022-09-18 17:36:36 +0200

    add obs avoidance

--- scripts/avoid_obs_from_scratch.py
+++ scripts/avoid_obs_from_scratch.py
@@ -8,6 +8,8 @@ from math import pi
 #from librairy of lukas : dynamic_obstacle_avoidance
 from dynamic_obstacle_avoidance.containers import ObstacleContainer
 from dynamic_obstacle_avoidance.obstacles import CuboidXd as Cuboid
+from dynamic_obstacle_avoidance.avoidance import ModulationAvoider
+from dynamic_obstacle_avoidance.visualization import plot_obstacles
 
 #from librairy of lukas : vartools
 from vartools.dynamical_systems import LinearSystem
@@ -22,7 +24,7 @@ class DynamicalSystemAnimation(Animator):
         self,
         initial_dynamics,
         obstacle_environment,
-        start_position=np.array([1, 1]),
+        start_position=np.array([-2.5, 0.5]),
         x_lim=[-1.5, 2],
         y_lim=[-0.5, 2.5],
     ):
@@ -32,7 +34,12 @@ class DynamicalSystemAnimation(Animator):
         self.obstacle_environment = obstacle_environment
         self.initial_dynamics = initial_dynamics
 
-        self.position_list = np.zeros((self.dim, self.it_max))
+        self.dynamic_avoider = ModulationAvoider(
+            initial_dynamics=self.initial_dynamics,
+            obstacle_environment=self.obstacle_environment,
+        )
+
+        self.position_list = np.zeros((self.dim, self.it_max + 1))
         self.position_list[:, 0] = start_position
 
         self.fig, self.ax = plt.subplots(figsize=(10, 8))
@@ -42,10 +49,14 @@ class DynamicalSystemAnimation(Animator):
 
         #CALCULATION
         #ici diférent de lukas : moi ii et ii + 1 (lui ii -1 et ii)
-        velocity = self.initial_dynamics.evaluate(self.position_list[:,ii])
+
+        velocity = self.dynamic_avoider.evaluate(self.position_list[:, ii])
         self.position_list[:, ii + 1] = (
             velocity * self.dt_simulation + self.position_list[:, ii]
         )
+
+        # Update obstacles
+        self.obstacle_environment.do_velocity_step(delta_time=self.dt_simulation)
 
         #CLEARING
         self.ax.clear()
@@ -75,7 +86,13 @@ class DynamicalSystemAnimation(Animator):
         )
 
         #obstacles positions
-        #...
+        plot_obstacles(
+            ax=self.ax,
+            obstacle_container=self.obstacle_environment,
+            x_lim=self.x_lim,
+            y_lim=self.y_lim,
+            showLabel=False,
+        )
 
         self.ax.grid(True)
         self.ax.set_aspect("equal", adjustable="box")
@@ -90,10 +107,23 @@ def run_obs_avoidance():
     obstacle_environment.append(
         Cuboid(
             axes_length=[0.4, 1.3],
-            center_position=np.array([2.2, 0.25]),
+            center_position=np.array([0.0, 0.25]),
             # center_position=np.array([0.9, 0.25]),
-            margin_absolut=0.5,
-            orientation=10 * pi / 180,
+            margin_absolut=0.2,
+            # orientation=10 * pi / 180,
+            linear_velocity = np.array([-1.0, 0.0]),
+            tail_effect=False,
+            # repulsion_coeff=1.4,
+        )
+    )
+    obstacle_environment.append(
+        Cuboid(
+            axes_length=[1.3, 0.4],
+            center_position=np.array([-1.0, -0.5]),
+            # center_position=np.array([0.9, 0.25]),
+            margin_absolut=0.2,
+            # orientation=10 * pi / 180,
+            linear_velocity = np.array([0.0, 1.0]),
             tail_effect=False,
             # repulsion_coeff=1.4,
         )
@@ -102,11 +132,12 @@ def run_obs_avoidance():
 
     #setup of dynamics
     initial_dynamics = LinearSystem(
-        attractor_position=np.array([0.0, 0.0]),
-        maximum_velocity=1,
+        attractor_position=np.array([2.0, 0.0]),
+        maximum_velocity=2,
         distance_decrease=0.3,
     )
 
+    #setup of animator
     my_animation = DynamicalSystemAnimation(
         dt_simulation=0.05,
         dt_sleep=0.01,
@@ -120,10 +151,6 @@ def run_obs_avoidance():
     )
 
     my_animation.run(save_animation=False)
-
-
-
-    pass
 
 
 if (__name__) == "__main__":
