f9785dae6fda25a84b809e96bac82a903ce8a40b
TicaGit <thibaud.trinca@gmail.com>
2022-12-21 18:25:56 +0100

    work on real robot

--- docker_scripts/scripts/docker_passive_control.py
+++ docker_scripts/scripts/docker_passive_control.py
@@ -18,8 +18,9 @@ from scipy.spatial.transform import Rotation as R
 from franka_avoidance.robot_interface import RobotZmqInterface as RobotInterface
 from dynamic_obstacle_avoidance.avoidance.base_avoider import BaseAvoider
 
-# My custom librairies 
+# My custom librairies
 from librairies.docker_helper import Simulated
+
 
 class PassiveObsController(Node):
     def __init__(self, robot, freq: float = 100, node_name="passive_controller"):
@@ -30,52 +31,55 @@ class PassiveObsController(Node):
         self.command = CommandMessage()
         self.command.control_type = [ControlType.EFFORT.value]
 
-        #initialize controller : Fcomannd = I*x_dd + D*x_d + K*x
+        # initialize controller : Fcomannd = I*x_dd + D*x_d + K*x
         self.ctrl = create_cartesian_controller(CONTROLLER_TYPE.IMPEDANCE)
-        self.ctrl.set_parameter_value("damping", np.zeros((6,6)), sr.ParameterType.MATRIX)
-        self.ctrl.set_parameter_value("stiffness", np.zeros((6,6)), sr.ParameterType.MATRIX)
-        self.ctrl.set_parameter_value("inertia", np.zeros((6,6)), sr.ParameterType.MATRIX)
+        self.ctrl.set_parameter_value(
+            "damping", np.eye(6), sr.ParameterType.MATRIX)
+        self.ctrl.set_parameter_value(
+            "stiffness", np.eye(6), sr.ParameterType.MATRIX)
+        self.ctrl.set_parameter_value(
+            "inertia", np.eye(6), sr.ParameterType.MATRIX)
 
-        #usefull to print
-        #print("methods : ", dir(self.ctrl))
-        #print("PARAMS CTRL : ", self.ctrl.get_parameters())
+        # usefull to print
+        # print("methods : ", dir(self.ctrl))
+        # print("PARAMS CTRL : ", self.ctrl.get_parameters())
 
-        #sim is an instance containing the virtual copy of all the elements, used to compute D
+        # sim is an instance containing the virtual copy of all the elements, used to compute D
         self.sim = Simulated(
-            lambda_DS = 200.0, #100
-            lambda_perp = 20.0, #20
-            lambda_obs = 200, #200
+            lambda_DS=10.0,  # 100
+            lambda_perp=1.0,  # 20
+            lambda_obs=10,  # 200
         )
 
-        #create obstacle env
-        obs_position = np.array([[0.5, 0.0, 0.25]]) #these are N x 3
-        obs_axes_lenght = np.array([[0.25] *3]) #for plot need to be all equal -> sphere, its the radius
+        # create obstacle env
+        obs_position = np.array([[0.0, 0.0, 0.0]])  # these are N x 3
+        # for plot need to be all equal -> sphere, its the radius
+        obs_axes_lenght = np.array([[0.20] * 3])
         obs_vel = np.array([[0.0] * 3])
-        no_obs = False #to disable obstacles
-        self.obstacle_env = self.sim.create_env(obs_position, obs_axes_lenght, obs_vel, no_obs)
+        no_obs = True  # to disable obstacles
+        self.obstacle_env = self.sim.create_env(
+            obs_position, obs_axes_lenght, obs_vel, no_obs)
 
-        #create the DS, NOT FORGET minus !!! (!= cpp )
+        # create the DS, NOT FORGET minus !!! (!= cpp )
         self.A_matrix = -np.diag([1.0, 1.0, 1.0])
-        self.attractor_A = np.array([0.5, 0.5, 0.3])
-        self.attractor_B = np.array([0.5, -0.5, 0.3])
+        self.attractor_A = np.array([0.4, 0.3, 0.4])
+        self.attractor_B = np.array([0.4, -0.3, 0.4])
         self.attractor_position = self.attractor_A
-        self.attrator_quaternion = np.array([0.0, 0.0, 1.0, 0.0])
-        self.max_vel = 0.5
-        self.sim.create_DS_copy(self.attractor_position, self.A_matrix, self.max_vel)
+        self.attrator_quaternion = np.array([0.0, 1.0, 0.0, 0.0])
+        self.max_vel = 0.2
+        self.sim.create_DS_copy(self.attractor_position,
+                                self.A_matrix, self.max_vel)
 
-        #create modulation avoider to modulate final DS
+        # create modulation avoider to modulate final DS
         self.sim.create_mod_avoider()
 
-        #to compare - remooove when debuged
+        # to compare - remooove when debuged
         # self.ctrl_ref = create_cartesian_controller(CONTROLLER_TYPE.DISSIPATIVE)
         # self.ctrl_ref.set_parameter_value("eigenvalues", np.diag([20, 2, 2, 1, 1, 1]), sr.ParameterType.MATRIX)
         # self.ds = create_cartesian_ds(DYNAMICAL_SYSTEM_TYPE.POINT_ATTRACTOR)
         # self.ds.set_parameter_value(
         #      "gain", [1, 1, 1, 0.1, 0.1, 0.1], sr.ParameterType.DOUBLE_ARRAY #standart linear DS
         # )
-
-
-
 
     def run(self):
         target_set = False
@@ -87,7 +91,7 @@ class PassiveObsController(Node):
                 continue
             if False:
                 pass
-            #below, usefull when ds in in cpp
+            # below, usefull when ds in in cpp
             # if not target_set:
             #     target = sr.CartesianPose(
             #         state.ee_state.get_name(),
@@ -104,76 +108,105 @@ class PassiveObsController(Node):
             #     #print("### DS ###", self.ds.get_parameters())
             #     target_set = True
             else:
-                ### CONTROL LOOP ### 
+                ### CONTROL LOOP ###
 
-                #0. get the pose from the state
-                x = state.ee_state.get_pose()   
+                # 0. get the pose from the state
+                x = state.ee_state.get_pose()
 
-                #extract the position and velocity
+                # extract the position and velocity
                 pos = x[0:3]
-                print(pos)
+                # print(pos)
                 pos_dot = state.ee_state.get_linear_velocity()
 
-                #extract the angle
-                r = R.from_quat(x[3:7])
-                ang = r.as_euler('zyx', degrees=False) #not sure which format
+                # extract the angle
+                # r = R.from_quat(x[3:7])
+                # ang = r.as_euler('zyx', degrees=False)  # not sure which format
 
-
-                #1. compute desired velocity in cartesian coord
+                # 1. compute desired velocity in cartesian coord
                 pos_dot_des = self.sim.dynamic_avoider.evaluate(pos)
-    
-                #print("actual position : ", pos, " and ang : ", x[3:7], " actual vel : ", pos_dot, " des_vel by avoider", pos_dot_des)
 
-                #2. compute the damping matrix and gains
+                # max_pos_des = max(abs(pos_dot_des))
+                # if max_pos_des > 0.1:
+                #     pos_dot_des = pos_dot_des/max_pos_des
+
+                print("actual position : ", pos, " and ang : ",
+                      x[3:7], " actual vel : ", pos_dot, " des_vel by avoider", pos_dot_des)
+
+                # 2. compute the damping matrix and gains
                 D = self.sim.compute_D(pos, pos_dot, pos_dot_des)
-                KD_ang = np.diag([1] *3)
-                big_D = np.zeros((6,6))
+                KD_ang = np.diag([5.0] * 3)  # 1
+                big_D = np.zeros((6, 6))
                 big_D[0:3, 0:3] = D
                 big_D[3:6, 3:6] = KD_ang
+                print('D ', big_D)
 
-                #3. build the K matrix
-                big_K = np.zeros((6,6)) 
-                KP_ang = np.diag([50] *3)
+                # 3. build the K matrix
+                big_K = np.zeros((6, 6))
+                KP_ang = np.diag([5.0] * 3)  # 5
                 big_K[3:6, 3:6] = KP_ang
+                print('K ', big_K)
 
-                #3. assign them to the robot
-                self.ctrl.set_parameter_value("damping", big_D, sr.ParameterType.MATRIX)
-                self.ctrl.set_parameter_value("stiffness", big_K, sr.ParameterType.MATRIX)
+                # 3. assign them to the robot
+                self.ctrl.set_parameter_value(
+                    "damping", big_D, sr.ParameterType.MATRIX)
+                self.ctrl.set_parameter_value(
+                    "stiffness", big_K, sr.ParameterType.MATRIX)
 
-                #print("## PARAMS CTRL NEW ## : ", self.ctrl.get_parameters())
+                # print("## PARAMS CTRL NEW ## : ", self.ctrl.get_parameters())
 
-                #4. construct des_ee_state
+                # 4. construct des_ee_state
                 des_ee_state = sr.CartesianState(
                     state.ee_state.get_name(),
                     state.ee_state.get_reference_frame(),
                 )
-                des_ee_state.set_pose(np.concatenate((np.zeros(3), self.attrator_quaternion))) #vector 7x1
+                des_ee_state.set_pose(np.concatenate(
+                    (np.zeros(3), self.attrator_quaternion)))  # vector 7x1
                 des_ee_state.set_linear_velocity(pos_dot_des)
                 des_ee_state.set_angular_velocity(np.zeros(3))
 
-                #5. compute the command with the impedance controller
+                # 5. compute the command with the impedance controller
                 self.command_torques = sr.JointTorques(
-                    self.ctrl.compute_command(des_ee_state, state.ee_state, state.jacobian) #SWAPED !!! ??
+                    self.ctrl.compute_command(
+                        des_ee_state, state.ee_state, state.jacobian)
                 )
-                #print("\n com. torque :", self.command_torques.get_torques())
+                # print("\n com. torque :", self.command_torques.get_torques())
 
-                #6. send the ecommand to the robot
+                # 6. send the ecommand to the robot
                 self.command.joint_state = state.joint_state
-                self.command.joint_state.set_torques(self.command_torques.get_torques())
+
+                max_torque = max(abs(self.command_torques.get_torques()))
+                if max_torque > 15:
+                    print("torques were limited (>1)",
+                          self.command_torques.get_torques())
+                    self.command.joint_state.set_torques(
+                        self.command_torques.get_torques()/max_torque)
+                else:
+                    self.command.joint_state.set_torques(
+                        self.command_torques.get_torques())
+
+                # self.command_torques.get_torques()
+                # breakpoint()
+                # print("torques : ", self.command_torques.get_torques())
+
                 self.robot.send_command(self.command)
 
-                #7. draw obs
-                self.obstacle_env.update_obstacles()
+                # 7. draw obs
+                if self.obstacle_env is not None:
+                    # self.obstacle_env.update_obstacles()
+                    self.obstacle_env.update()
+                    print('opti ok')
 
-                #8. switch atractor if converged
+                # 8. switch atractor if converged
                 EPS = 1e-1
                 if atr_a:
-                    print("A")
+                    # print("A")
+                    pass
                 else:
-                    print("B")
-                #print("current atractor position : ", self.attractor_position)
+                    # print("B")
+                    pass
+                # print("current atractor position : ", self.attractor_position)
                 if np.linalg.norm(pos - self.attractor_position) <= EPS:
-                    #print("SWICH")
+                    # print("SWICH")
                     if atr_a:
                         self.attractor_position = self.attractor_B
                         atr_a = False
@@ -181,24 +214,23 @@ class PassiveObsController(Node):
                         self.attractor_position = self.attractor_A
                         atr_a = True
 
-                    #update the DS
-                    self.sim.create_DS_copy(self.attractor_position, self.A_matrix, self.max_vel)
+                    # update the DS
+                    self.sim.create_DS_copy(
+                        self.attractor_position, self.A_matrix, self.max_vel)
 
-                    #update the modulation avoider to modulate final DS
+                    # update the modulation avoider to modulate final DS
                     self.sim.create_mod_avoider()
 
-
-                #comparison with cpp#
-                #ds_ret = self.ds.evaluate(state.ee_state)
+                # comparison with cpp#
+                # ds_ret = self.ds.evaluate(state.ee_state)
                 # sr.CartesianTwist
                 # com_ref = sr.JointTorques(
                 #     self.ctrl_ref.compute_command(ds_ret, state.ee_state, state.jacobian)
                 # )
-                #self.command.joint_state.set_torques(com_ref.get_torques())
-                ##command_torques has name 1->7 of joint + torque array 1x7
-                #self.robot.send_command(self.command)
+                # self.command.joint_state.set_torques(com_ref.get_torques())
+                # command_torques has name 1->7 of joint + torque array 1x7
+                # self.robot.send_command(self.command)
                 ###
-
 
             self.rate.sleep()
         self.obstacle_env.shutdown()
@@ -207,12 +239,14 @@ class PassiveObsController(Node):
 if __name__ == "__main__":
     rclpy.init()
     # rospy.init_node("test", anonymous=True)
-    robot_interface = RobotInterface("*:1601", "*:1602") #16 is for the first franka robot
+    # 16 is for the first franka robot
+    robot_interface = RobotInterface("*:1601", "*:1602")
 
     # Spin in a separate thread
     controller = PassiveObsController(robot=robot_interface, freq=500)
 
-    thread = threading.Thread(target=rclpy.spin, args=(controller,), daemon=True)
+    thread = threading.Thread(
+        target=rclpy.spin, args=(controller,), daemon=True)
     thread.start()
 
     try:
@@ -220,6 +254,6 @@ if __name__ == "__main__":
 
     except KeyboardInterrupt:
         pass
-    
+
     rclpy.shutdown()
     thread.join()
--- librairies/docker_helper.py
+++ librairies/docker_helper.py
@@ -2,7 +2,7 @@ import numpy as np
 import warnings
 
 
-#Lukas
+# Lukas
 from dynamic_obstacle_avoidance.containers import ObstacleContainer
 from dynamic_obstacle_avoidance.obstacles import CuboidXd as Cuboid
 from dynamic_obstacle_avoidance.obstacles import EllipseWithAxes as Ellipse
@@ -12,36 +12,35 @@ from dynamic_obstacle_avoidance.utils import get_orthogonal_basis
 
 from vartools.dynamical_systems import LinearSystem
 
-#new for obs viz
+# new for obs viz
 from franka_avoidance.optitrack_container import OptitrackContainer
 from franka_avoidance.pybullet_handler import PybulletHandler
+from franka_avoidance.rviz_handler import RvizHandler
 
 
 class Simulated():
     def __init__(
         self,
-        lambda_DS = 100.0,
-        lambda_perp = 20.0,
-        lambda_obs = 200,
+        lambda_DS=100.0,
+        lambda_perp=20.0,
+        lambda_obs=200,
     ):
         self.lambda_DS = lambda_DS
         self.lambda_perp = lambda_perp
         self.lambda_obs = lambda_obs
         self.D = np.diag([self.lambda_DS, self.lambda_perp, self.lambda_obs])
 
-
-
     def create_env(self, obs_pos, obs_axes_lenght, obs_vel, no_obs):
-        #old
-        #self.obstacle_environment = ObstacleContainer()
+        # old
+        # self.obstacle_environment = ObstacleContainer()
 
-        #new
-        self.obstacle_environment = OptitrackContainer(use_optitrack=False)
+        # new
+        self.obstacle_environment = OptitrackContainer(use_optitrack=True)
 
         if no_obs:
             return
         for i, (pos, axes, vel) in enumerate(zip(obs_pos, obs_axes_lenght, obs_vel)):
-            #old
+            # old
             # self.obstacle_environment.append(
             #     Ellipse(
             #         axes_length = axes,
@@ -54,49 +53,49 @@ class Simulated():
             #     )
             # )
 
-            #new
+            # new
             self.obstacle_environment.append(
                 Ellipse(
-                    center_position = pos,
-                    axes_length = axes,
-                    linear_velocity = vel,
+                    center_position=pos,
+                    axes_length=axes,
+                    linear_velocity=vel,
                 ),
-                obstacle_id=i,
+                obstacle_id=27,
             )
-         
-        self.obstacle_environment.visualization_handler = PybulletHandler(self.obstacle_environment)
+
+        # self.obstacle_environment.visualization_handler = PybulletHandler(self.obstacle_environment)
+        self.obstacle_environment.visualization_handler = RvizHandler(
+            self.obstacle_environment)
+
         return self.obstacle_environment
-            
-        
 
     def create_DS_copy(self, attractor_position, A_matrix, max_vel):
         self.initial_dynamics = LinearSystem(
-            attractor_position = attractor_position,
-            A_matrix = A_matrix,
+            attractor_position=attractor_position,
+            A_matrix=A_matrix,
             dimension=3,
             maximum_velocity=max_vel,
-            distance_decrease=0.1, #if too small, could lead to instable around atractor 
+            distance_decrease=0.1,  # if too small, could lead to instable around atractor
         )
 
     def create_mod_avoider(self):
-            self.dynamic_avoider = ModulationAvoider(
-                initial_dynamics = self.initial_dynamics,
-                obstacle_environment = self.obstacle_environment,
-            )
+        self.dynamic_avoider = ModulationAvoider(
+            initial_dynamics=self.initial_dynamics,
+            obstacle_environment=self.obstacle_environment,
+        )
 
     def compute_D_matrix_wrt_DS(self, x_dot_des):
-        #print("compute D wrt DS only")
-        #construct the basis matrix, align to the DS direction 
+        # print("compute D wrt DS only")
+        # construct the basis matrix, align to the DS direction
         E = get_orthogonal_basis(x_dot_des)
         E_inv = np.linalg.inv(E)
 
+        # contruct the matrix containing the damping coefficient along selective directions
+        lambda_mat = np.array([[self.lambda_DS, 0.0, 0.0],
+                               [0.0, self.lambda_perp, 0.0],
+                               [0.0, 0.0, self.lambda_perp]])
 
-        #contruct the matrix containing the damping coefficient along selective directions
-        lambda_mat = np.array([[self.lambda_DS, 0.0, 0.0], 
-                                [0.0, self.lambda_perp, 0.0],
-                                [0.0, 0.0, self.lambda_perp]])
-
-        #compose the damping matrix
+        # compose the damping matrix
         D = E@lambda_mat@E_inv
         self.D = D
         return D
@@ -106,66 +105,64 @@ class Simulated():
         DIM = 3
         DIST_CRIT = 1
 
-        #if there is no obstacles, we want D to be stiff w.r.t. the DS
+        # if there is no obstacles, we want D to be stiff w.r.t. the DS
         if not len(self.obstacle_environment):
-            #print("no obs")
+            # print("no obs")
             return self.compute_D_matrix_wrt_DS(x_dot_des)
 
-        #print("compute D wrt obs")
-        #KEEP ? if the desired velocity is too small, we risk numerical issue
+        # print("compute D wrt obs")
+        # KEEP ? if the desired velocity is too small, we risk numerical issue
         if np.linalg.norm(x_dot_des) < EPSILON:
-            #we just return the previous damping matrix
+            # we just return the previous damping matrix
             return self.D
-        
-        #compute the vector align to the DS
+
+        # compute the vector align to the DS
         e1_DS = x_dot_des/np.linalg.norm(x_dot_des)
 
-
-
-        #get the normals and distance to the obstacles
+        # get the normals and distance to the obstacles
         obs_normals_list = np.empty((DIM, 0))
         obs_dist_list = np.empty(0)
         for obs in self.obstacle_environment:
-            #gather the parameters wrt obstacle i
-            normal = obs.get_normal_direction(x, in_obstacle_frame = False).reshape(DIM, 1)
+            # gather the parameters wrt obstacle i
+            normal = obs.get_normal_direction(
+                x, in_obstacle_frame=False).reshape(DIM, 1)
             obs_normals_list = np.append(obs_normals_list, normal, axis=1)
 
-            d = obs.get_gamma(x, in_obstacle_frame = False) - 1
+            d = obs.get_gamma(x, in_obstacle_frame=False) - 1
             obs_dist_list = np.append(obs_dist_list, d)
 
-
-        #compute vector relative to obstacles
+        # compute vector relative to obstacles
         weight = 0
         e2_obs = np.zeros(DIM)
-        #compute the weight of the obstacles and resulting normal
+        # compute the weight of the obstacles and resulting normal
         for normal, dist in zip(obs_normals_list.T, obs_dist_list):
-            #if dist <0 we're IN the obs
-            if dist <= 0: 
+            # if dist <0 we're IN the obs
+            if dist <= 0:
                 return self.D
 
-            #weight is 1 at the boundary, 0 at a distance DIST_CRIT from the obstacle
-            #keep only biggest wight -> closer obstacle
+            # weight is 1 at the boundary, 0 at a distance DIST_CRIT from the obstacle
+            # keep only biggest wight -> closer obstacle
             weight_i = max(0.0, 1.0 - dist/DIST_CRIT)
             if weight_i > weight:
                 weight = weight_i
 
-            #e2_obs is a weighted linear combianation of all the normals
+            # e2_obs is a weighted linear combianation of all the normals
             e2_obs += normal/(dist + 1)
 
         e2_obs_norm = np.linalg.norm(e2_obs)
         if not e2_obs_norm:
-            #resultant normal not defined
-            #what to do ?? -> return prev mat
+            # resultant normal not defined
+            # what to do ?? -> return prev mat
             return self.D
         e2_obs = e2_obs/e2_obs_norm
 
         # compute the basis of the DS : [e1_DS, e2_DS] or 3d
         e3 = np.cross(e1_DS, e2_obs)
         norm_e3 = np.linalg.norm(e3)
-        if not norm_e3: #limit case if e1_DS//e2_obs -> DS aligned w/ normal
+        if not norm_e3:  # limit case if e1_DS//e2_obs -> DS aligned w/ normal
             warnings.warn("Limit case")
-            return self.D #what to do ??
-        else : 
+            return self.D  # what to do ??
+        else:
             e3 = e3/norm_e3
         e2_DS = np.cross(e3, e1_DS)
         e1_obs = np.cross(e2_obs, e3)
@@ -177,37 +174,36 @@ class Simulated():
         if np.dot(e1_DS, e1_obs) < 0:
             e1_obs = -e1_obs
 
-        #we construct a new basis which is always othtonormal but "rotates" based of weight
+        # we construct a new basis which is always othtonormal but "rotates" based of weight
         e1_both = weight*e1_obs + (1-weight)*e1_DS
         e1_both = e1_both/np.linalg.norm(e1_both)
 
         e2_both = weight*e2_obs + (1-weight)*e2_DS
         e2_both = e2_both/np.linalg.norm(e2_both)
 
-        #extreme case sould never happen, e1, e2 are suposed to be a orthonormal basis
+        # extreme case sould never happen, e1, e2 are suposed to be a orthonormal basis
         if 1 - np.abs(np.dot(e1_both, e2_both)) < EPSILON:
-            raise("What did trigger this, it shouldn't")
-            
-        #construct the basis matrix
+            raise ("What did trigger this, it shouldn't")
+
+        # construct the basis matrix
         E = np.array([e1_DS, e2_both, e3]).T
         E_inv = np.linalg.inv(E)
 
-        #compute the damping coeficients along selective directions
-        lambda_1 = self.lambda_DS #(1-weight)*self.lambda_DS #good ??
+        # compute the damping coeficients along selective directions
+        lambda_1 = self.lambda_DS  # (1-weight)*self.lambda_DS #good ??
         lambda_2 = (1-weight)*self.lambda_perp + weight*self.lambda_obs
         lambda_3 = self.lambda_perp
 
-        #if we go away from obs, we relax the stiffness
+        # if we go away from obs, we relax the stiffness
         if np.dot(xdot, e2_obs) > 0:
             lambda_2 = self.lambda_perp
 
-        #contruct the matrix containing the damping coefficient along selective directions
-        lambda_mat = np.array([[lambda_1, 0.0, 0.0], 
-                                [0.0, lambda_2, 0.0],
-                                [0.0, 0.0, lambda_3]])
-        
-        #compose the damping matrix
+        # contruct the matrix containing the damping coefficient along selective directions
+        lambda_mat = np.array([[lambda_1, 0.0, 0.0],
+                               [0.0, lambda_2, 0.0],
+                               [0.0, 0.0, lambda_3]])
+
+        # compose the damping matrix
         D = E@lambda_mat@E_inv
         self.D = D
         return D
-
